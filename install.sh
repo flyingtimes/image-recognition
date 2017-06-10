af1e4f3e60938384180c9a9247a66cbf0f432b2c
flyingtimes <longvocation@qq.com>
2017-06-11 00:39:51 +0800

    modified:   install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,10 @@
 #!/bin/sh
-if [ -n `docker`]; then
+if [ ! -f "/proc/cpuinfo" ]; then
+  echo '只能在树莓派安装本程序.'
+  exit
+fi
+if not which docker 2>/dev/null;then
  echo '没有检测到docker，需要安装docker......'
 else
- echo '检测到已经安装docker'
+ echo '检测到已经安装docker.'
 fi
