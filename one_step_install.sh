02cf878942bb12bcda11d046d0278e80dfc0e628
flyingtimes <5200374@qq.com>
2018-06-03 13:34:29 +0800

    Update one_step_install.sh

--- one_step_install.sh
+++ one_step_install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "从 github 上 拉取程序代码.."
-git clone https://github.com/flyingtimes/image-recognition.git
+git clone  --depth=1 https://github.com/flyingtimes/image-recognition.git
 cd image-recognition
 bash install.sh
 
